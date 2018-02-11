de97900c5e63d29292c52f68ca1b3319891a1663
khalidsakib <knsakib@gmail.com>
2018-02-11 14:28:02 -0500

    more contents about cloud and devops

--- src/Gitcheat.js
+++ src/Gitcheat.js
@@ -11,118 +11,120 @@ class Gitcheat extends Component {
           cheatsheet on Docker.
         </p>
 
-        <p>
-          <h3>Configuring GitHub</h3>
+
+          <h4>Configuring GitHub</h4>
+          We need to confiure both username and password as next step after installation
           <code>
-            git config --global user.name "user_name"<br />
-            git config --global user.email "email_id"
+            <br />git config --global user.name "user_name"
+            <br />git config --global user.email "email_id"<br />
           </code>
-          <h3>Creating a local repository</h3>{" "}
+          <h4>Creating a local repository</h4>{" "}
           <code>git init myCodeFolder</code>
-          <h3>
+          <h4>
             Adds the files in the local repository and stages them for commit
-          </h3>
+          </h4>
           <code>git add .</code>
-          <h3>Committing changes made to the index</h3>
+          <h4>Committing changes made to the index</h4>
           <code>git commit -m "some_message"</code>
-          <h3>
+          <h4>
             Connect the remote repository to the local repository. First the
-            remote repository should be created with the same of the local
+            remote repository should be created with the same name of the local
             repository{" "}
-          </h3>
+          </h4>
           <code>
             git remote add origin https://github.com/user_name/myCodeFolder.git
           </code>
-          <h3>Pushing files in local repository to GitHub repository</h3>
+          <h4>Pushing files in local repository to GitHub repository</h4>
           <code>git push origin master</code>
-          <h3>To unstage a file</h3>
+          <h4>To unstage a file</h4>
           <code>git reset HEAD FILE_NAME</code>
-          <h3>Merge development branch with master</h3>
+          <h4>Merge development branch with master</h4>
+          These are first work with branch and after finish merge to master
           <code>
-            git branch development <br />
-            git add * <br />
-            git commit -m "My commit message" <br />
-            git push -u origin development <br />{" "}
+            <br />git branch development
+            <br />git add *
+            <br />git commit -m "My commit message"
+            <br />git push -u origin development <br />
           </code>
-          <p>
+
             Now I will merge all the changes on the development branch into the
             master
             <code>
-              <br /> git checkout master <br />
-              git merge development <br />
-              git push -u origin master
+              <br /> git checkout master
+              <br />git merge development
+              <br />git push -u origin master<br />
             </code>
-          </p>
-          <h3>To remove the commit(before push) and to modify the file </h3>
+
+          <h4>To remove the commit(before push) and to modify the file </h4>
           <code>git reset --soft HEAD~1</code>
-          <h3>
+          <h4>
             Setting your branch to exactly match the remote branch can be done
             in two steps
-          </h3>
+          </h4>
           <code>
-            git fetch origin <br />
-            git reset --hard origin/master
+            git fetch origin
+            <br />git reset --hard origin/master<br />
           </code>
-          <h3>
-            I you want to save your current branch's state other than master
-          </h3>
+          <h4>
+            I you want to save my current branch's state other than master
+          </h4>
           <code>
-            git commit -a -m "Saving my work, just in case" <br />
-            git branch my-saved-work
+            git commit -a -m "Saving my work, just in case"
+            <br />git branch my-saved-work<br />
           </code>
-          <h3>When to use git stash</h3>
-          <p>
+          <h4>When to use git stash</h4>
+
             When I have changes on the working copy, git pull will not work.
             This is mainly to stash the changes in a dirty working directory
             away. In other words, when I want to record the current state of the
             working directory and the index, but want to go back to a clean
             working directory.
-          </p>
+
           <code>git stash</code>
-          <h3>
+          <h4>
             When I want to pull the new changes to my local from the remote.{" "}
-          </h3>
-          <p>
+          </h4>
+
             This command incorporates changes from a remote repository into the
             current branch. It will pull changes from upstream branch. In its
             default mode, git pull is shorthand for
-            <code>git fetch</code> followed by{" "}
+            <code>git fetch</code> followed by
             <code>git merge FETCH_HEAD.</code>{" "}
-          </p>
+
           <code>git pull //this will from the master </code>
-          <p>
+
             Pull before before making change in the local. Do not pull after
             making change, otherwise I will have conflicts
-          </p>
-          <h3>If I want to back to working copy from sstashed copy</h3>.
+
+          <h4>If I want to back to working copy from sstashed copy</h4>.
           <code>git stash pop</code>
-          <p>
+
             This will apply stashed changes back to working copy unless there is
             conflicts. In the case of conflict, they will stay in stash.
-          </p>
-          <h3>To resolve conflict</h3>
-          <p>
+
+          <h4>To resolve conflict</h4>
+
             <code>git staus</code> to show where is the conflicts. Resolve and
-            commit.{" "}
-          </p>
-          <h3>There is another better way to merge development</h3>
-          <p>
-            {" "}
+            commit.
+
+          <h4>There is another better way to merge development</h4>
+
+
             merge master into the development first so that if there are any
             conflicts, it can be resolved in the development branch itself and
             master remains clean.
             <code>
-              {" "}
-              git branch development <br /> git merge master (resolve any merge
-              conflicts if there are any)<br />
-              git add * <br />
-              git commit -m "My commit message" <br />
-              git push -u origin development <br />
-              git checkout master <br />
-              git merge --no-ff development (there won't be any conflicts now){" "}
-              <br />
-              git push -u origin master
-            </code>{" "}
+
+              <br />git branch development
+              <br />git merge master (resolve any merge
+              <br />conflicts if there are any)
+              <br />git add *
+              <br />git commit -m "My commit message"
+              <br />git push -u origin development
+              <br />git checkout master
+              <br />git merge --no-ff development (there won't be any conflicts now)
+              <br />git push -u origin master<br />
+            </code>
             <br />
             These steps are sometimes to leave master untouched until final
             stuff.
@@ -132,8 +134,8 @@ class Gitcheat extends Component {
             merge master into development (first step) multiple times in my
             workflow, and creating a commit node for these might not be very
             useful.
-          </p>
-        </p>
+
+
       </div>
     );
   }
